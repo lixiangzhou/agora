7221e50500db8661b6f48e4c073bce474d8eeeff
lixz <lixiangzhouno1@163.com>
2019-07-17 10:14:14 +0800

    添加代理方法

--- AgoraSingleCall/AgoraSingleCall/SingleCall/AgoraVideoCallManager.swift
+++ AgoraSingleCall/AgoraSingleCall/SingleCall/AgoraVideoCallManager.swift
@@ -108,29 +108,91 @@ extension AgoraVideoCallManager {
 }
 
 extension AgoraVideoCallManager: AgoraRtcEngineDelegate {
+    /// 警告：开发者可以忽略
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didOccurWarning warningCode: AgoraWarningCode) {
+        print(#function, warningCode.rawValue)
+    }
     
+    /// 错误：无法忽略
     func rtcEngine(_ engine: AgoraRtcEngineKit, didOccurError errorCode: AgoraErrorCode) {
         print(#function, errorCode.rawValue)
         if errorCode != .noError {
             callStatus = .hangupUnNormal
+        } else {
+            callStatus = .hangupNormal
         }
+    }
+    
+    /// SDK 执行的方法
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didApiCallExecute error: Int, api: String, result: String) {
+        print(#function, api, error, result)
+    }
+    
+    /// 本地用户加入 channel, 与 joinSuccessBlock 一样
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didJoinChannel channel: String, withUid uid: UInt, elapsed: Int) {
+        print(#function, channel, uid, elapsed)
+    }
+    
+    /// 本地用户重新加入频道
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didRejoinChannel channel: String, withUid uid: UInt, elapsed: Int) {
+        print(#function, channel, uid, elapsed)
+    }
+    
+    /// 本地用户离开 channel
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didLeaveChannelWith stats: AgoraChannelStats) {
+        print(#function, stats)
+    }
+    
+    /// 本地注册了一个用户
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didRegisteredLocalUser userAccount: String, withUid uid: UInt) {
+        print(#function, userAccount, uid)
+    }
+    
+    /// 获取远端用户和ID时调用
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didUpdatedUserInfo userInfo: AgoraUserInfo, withUid uid: UInt) {
+        print(#function, userInfo, uid)
+    }
+    
+    /// 本地用户切换为 直播时 调用，（调用setClientRole时调用）
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didClientRoleChanged oldRole: AgoraClientRole, newRole: AgoraClientRole) {
+        print(#function, oldRole, newRole)
+    }
+    
+    /// 远端加入了 channel，或重新加入 channel
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didJoinedOfUid uid: UInt, elapsed: Int) {
+        print(#function, uid, elapsed)
+        callStatus = .active
+    }
+    
+    /// 远端离开了 channel
+    func rtcEngine(_ engine: AgoraRtcEngineKit, didOfflineOfUid uid:UInt, reason:AgoraUserOfflineReason) {
+        print(#function, uid, reason.rawValue)
+        rtcEngineDidOfflineOfUidClosure?(engine, Int(uid), reason)
+    }
+    
+    /// 连接状态改变
+    func rtcEngine(_ engine: AgoraRtcEngineKit, connectionChangedTo state: AgoraConnectionStateType, reason: AgoraConnectionChangedReason) {
+        print(#function, state.rawValue, reason.rawValue)
+    }
+    
+    /// 本地网络连接状态改变
+    func rtcEngine(_ engine: AgoraRtcEngineKit, networkTypeChangedTo type: AgoraNetworkType) {
+        print(#function, type.rawValue)
+    }
+    
+    ///
+    func rtcEngineConnectionDidLost(_ engine: AgoraRtcEngineKit) {
+        print(#function)
     }
     
     func rtcEngine(_ engine: AgoraRtcEngineKit, firstRemoteVideoDecodedOfUid uid:UInt, size:CGSize, elapsed:Int) {
         print(#function)
         rtcEngineFirstRemoteVideoDecodedOfUidClosure?(engine, uid)
-        
     }
     
-    func rtcEngine(_ engine: AgoraRtcEngineKit, didJoinedOfUid uid: UInt, elapsed: Int) {
-        print(#function)
-        callStatus = .active
-    }
     
-    func rtcEngine(_ engine: AgoraRtcEngineKit, didOfflineOfUid uid:UInt, reason:AgoraUserOfflineReason) {
-        print(#function)
-        rtcEngineDidOfflineOfUidClosure?(engine, Int(uid), reason)
-    }
+    
+    
     
     func rtcEngine(_ engine: AgoraRtcEngineKit, reportRtcStats stats: AgoraChannelStats) {
         print(#function, stats.duration)
